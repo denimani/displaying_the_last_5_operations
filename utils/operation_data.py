dcee69c714b06b1e46abe6b247cf40d34af97247
denimani <isxakov.den@bk.ru>
2023-03-22 13:28:33 +0300

    Added file with 5 last successful operations

---
+++ utils/operation_data.py
@@ -0,0 +1,22 @@
+import json
+
+
+def open_operations_file():
+    with open('operations.json', encoding='utf-8') as f:
+        return json.load(f)
+
+
+def last_five_operations():
+    """
+    Функция возвращает список последних 5 успешных операций из всего списка операций пользователя.
+    :return: список последних 5 успешных операций
+    """
+    list_executed_operations = []
+    count = 0
+    for operation in open_operations_file():
+        if operation['state'] == 'EXECUTED':
+            count += 1
+            list_executed_operations.append(operation)
+            if count == 5:
+                return list_executed_operations
+
